15432692a475fa0f08aea715964a86fd4ec86d08
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-07-21 14:03:10 +0100

    Mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,15 +3,26 @@
 import sys
 
 
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-       self.reg = [0] * 8
+        self.reg = [0] * 8
         self.ram = [0] * 256
         self.pc = 0
         self.halted = False
+
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
+
+    def ram_read(self, mar):
+        return self.ram[mar]
 
     def load(self):
         """Load a program into memory."""
@@ -22,18 +33,17 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -66,6 +76,25 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+        instruction_length = 1  # (bitshifted instruction)
         while not self.halted:
             ir = self.ram[self.pc]
-            operand_a = 
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # set the instruction length here (extract)
+
+            # halt
+            if ir == HLT:
+                self.halted = True
+
+            # LDI
+            elif ir == LDI:
+                self.reg[operand_a] = operand_b
+                instruction_length = 3
+
+            # PRN
+            elif ir == PRN:
+                print(self.reg[operand_a])
+                instruction_length = 2
+
+            self.pc += instruction_length
